776192b498b407bc532b91f610e6e5543dc4a89e
Pablo Lagger <pablo_lagger@hotmail.com>
2021-10-26 22:37:41 +0000

    se agrega metodo que evita que se duerma la instancia de dyno

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const wakeUpDyno = require ("./app/services/wakeUp/wakeUpDyno");
 
 const app = express();
 
@@ -25,5 +26,6 @@ require("./app/routes/index.routes")(app);
 
 const PORT = process.env.PORT || 15000;
 app.listen(PORT, () => {
+    wakeUpDyno("https://notflix-fya-backend.herokuapp.com/"); 
     console.log(`Server is running on port ${PORT}.`);
 });
